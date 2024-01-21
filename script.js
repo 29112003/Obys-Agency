353c45e97afabadedba003a20c6ccf75b39de488
keshav1120 <keshavgeek11@gmail.com>
2024-01-21 16:47:26 +0530

    loader animation

--- script.js
+++ script.js
@@ -1,30 +1,40 @@
-// loader 
-function loader(){
-    gsap.from(".loader .load h1",{
+
+var tl = gsap.timeline();
+
+tl.from(".timer",{
+    opacity : 0,
+    delay:0.4
+})
+tl.from(".loader .load h1,.load h2",{
         y : 140,
+        opacity : 0,
         duration:1,
-        delay:0.4,
         stagger:0.2,
     })
-
-    console.log(Math.floor(Math.random()*10));
+    tl.to(".load",{
+        opacity:0,
+        delay:1.6,
+        stagger:-0.2
+    })
+    tl.to(".loader",{
+        top:"-100%",
+        duration:1,
+        ease:"power4.out"
+    })
 
 var timer = document.querySelector(".timer h4")
 
 var counter = 0;
 var interval = setInterval(function(){
     if(counter <= 100){
-        counter+= Math.floor(Math.random()*10);
-        console.log(counter);
+        counter++;
         timer.innerHTML = counter;
     }else{
         timer.innerHTML = 100;
-
             clearInterval(interval);
     }
-},50)
-
-}
+},30)
 
 
-loader();
+
+Shery.mouseFollower();
